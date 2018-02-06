86336216bced6d2ac20abfd667ac2f20f992df9f
loveencounterflow <paragate@gmx.net>
2018-02-06 15:13:54 +0100

    better hostname splitting

--- python_modules/url_parser.py
+++ python_modules/url_parser.py
@@ -39,14 +39,28 @@ def split_words( text ):
   return [ r for r in R if r ]
 
 #-----------------------------------------------------------------------------------------------------------
+def split_hostname( hostname ):
+  *subdomains, sld, tld = hostname.split( '.' )
+  domain                = sld + '.' + tld
+  return [
+       [ 'url/tld',       tld,        ],
+       [ 'url/sld',       sld,        ],
+       [ 'url/domain',    domain,     ],
+    *[ [ 'url/subdomain', subdomain,  ] for subdomain in subdomains ], ]
+
+#-----------------------------------------------------------------------------------------------------------
 def parse_words( url_ ):
   R = []
   scheme, netloc, username, password, hostname, port, path, params, query, fragment = parse( url_ )
-  if username:  R.extend( [ [ 'url/username',   w, ] for w in split_words( username ) ] )
-  if password:  R.extend( [ [ 'url/password',   w, ] for w in split_words( password ) ] )
-  if hostname:  R.extend( [ [ 'url/hostname',   w, ] for w in split_words( hostname ) ] )
-  if path:      R.extend( [ [ 'url/path',       w, ] for w in split_words( path     ) ] )
-  if fragment:  R.extend( [ [ 'url/fragment',   w, ] for w in split_words( fragment ) ] )
+  if username:  R.extend( [ [ 'url/username',   w, ] for w in split_words(    username ) ] )
+  if password:  R.extend( [ [ 'url/password',   w, ] for w in split_words(    password ) ] )
+  if path:      R.extend( [ [ 'url/path',       w, ] for w in split_words(    path     ) ] )
+  if fragment:  R.extend( [ [ 'url/fragment',   w, ] for w in split_words(    fragment ) ] )
+  if hostname:  R.extend( split_hostname( hostname ) )
   return R
+
+
+
+
 
 
