6c77bce91cbadd8cb70ea7126a0bee3ad0151817
loveencounterflow <paragate@gmx.net>
2018-02-09 20:56:33 +0100

    update

--- db/070-catalog.sql
+++ db/070-catalog.sql
@@ -204,6 +204,59 @@ create view CATALOG.catalog as (
     select * from v1 order by t, schema, name
   );
 
+
+
+-- ---------------------------------------------------------------------------------------------------------
+drop schema if exists report cascade;
+create schema report;
+
+/* # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### ##  */
+/* # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### ##  */
+/* # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### # ## # ### ##  */
+/* thx to https://stackoverflow.com/a/46594226/7568091 */
+
+WITH RECURSIVE view_deps AS (
+SELECT DISTINCT dependent_ns.nspname as dependent_schema
+, dependent_view.relname as dependent_view
+, source_ns.nspname as source_schema
+, source_table.relname as source_table
+FROM pg_depend
+JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid
+JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid
+JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid
+JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
+JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
+WHERE NOT (dependent_ns.nspname = source_ns.nspname AND dependent_view.relname = source_table.relname)
+UNION
+SELECT DISTINCT dependent_ns.nspname as dependent_schema
+, dependent_view.relname as dependent_view
+, source_ns.nspname as source_schema
+, source_table.relname as source_table
+FROM pg_depend
+JOIN pg_rewrite ON pg_depend.objid = pg_rewrite.oid
+JOIN pg_class as dependent_view ON pg_rewrite.ev_class = dependent_view.oid
+JOIN pg_class as source_table ON pg_depend.refobjid = source_table.oid
+JOIN pg_namespace dependent_ns ON dependent_ns.oid = dependent_view.relnamespace
+JOIN pg_namespace source_ns ON source_ns.oid = source_table.relnamespace
+INNER JOIN view_deps vd
+    ON vd.dependent_schema = source_ns.nspname
+    AND vd.dependent_view = source_table.relname
+    AND NOT (dependent_ns.nspname = vd.dependent_schema AND dependent_view.relname = vd.dependent_view)
+)
+
+SELECT *
+FROM view_deps
+where true
+  and ( dependent_schema not in ( 'information_schema', 'pg_catalog' ) )
+ORDER BY
+  dependent_schema,
+  dependent_view,
+  source_schema,
+  source_table,
+  1;
+'xxxx integrate xxxx'
+
+
 -- ---------------------------------------------------------------------------------------------------------
 -- create view CATALOG.catalog_tsn  as select t, schema, name, remarks from CATALOG.catalog order by t, schema, name;
 -- create view CATALOG.catalog_stn  as select t, schema, name, remarks from CATALOG.catalog order by schema, t, name;
